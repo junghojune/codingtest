6e73eb1ae9f96cee883f65600e59a078a1707416
hosu <76641246+junghojune@users.noreply.github.com>
2022-11-11 17:35:58 +0900

    [Silver III] Title: N과 M (4), Time: 96 ms, Memory: 13384 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+    public static int m;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs(1,0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int at, int depth) {
+        if (depth == m) {
+            for (int k : arr) {
+                sb.append(k).append(' ');
+            }
+
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = at; i <= n; i++) {
+            arr[depth] = i;
+
+            dfs(i,depth + 1);
+
+        }
+    }
+}
