be54eb5d77587d5eb7e1a7c3704a7f7e1859c382
hosu <76641246+junghojune@users.noreply.github.com>
2022-11-16 17:39:57 +0900

    [Bronze V] Title: 팩토리얼, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(N));
+
+    }
+
+    public static int factorial(int N){
+        if(N <= 1){
+            return 1;
+        }
+        return N * factorial(N -1);
+    }
+}
