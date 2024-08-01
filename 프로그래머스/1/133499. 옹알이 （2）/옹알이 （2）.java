f8c7a50ab64a2e947010cc0ba2c6084c7ed5d99d
hosu <76641246+junghojune@users.noreply.github.com>
2024-08-01 10:22:30 +0900

    [level 1] Title: 옹알이 (2), Time: 6.07 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,28 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[] babbling) {
 
-        int answer = 0; // babbling 중 발음 가능한 단어 개수
-        
-        for(int i=0; i<babbling.length; i++){
-            // 연속발음될 경우 예외처리 
-            if(babbling[i].contains("ayaaya") 
-               || babbling[i].contains("yeye") 
-               || babbling[i].contains("woowoo")
-               || babbling[i].contains("mama")){
+        int answer = 0;
+
+        for (String s : babbling) {
+            if (s.contains("ayaaya") ||
+                    s.contains("yeye") ||
+                    s.contains("woowoo") ||
+                    s.contains("mama")
+            ) {
                 continue;
             }
-            
-            babbling[i] = babbling[i].replace("aya", " ");
-            babbling[i] = babbling[i].replace("ye", " ");
-            babbling[i] = babbling[i].replace("woo", " ");
-            babbling[i] = babbling[i].replace("ma", " ");
-            babbling[i] = babbling[i].replace(" ", "");
 
-            if(babbling[i].length()  == 0) answer++;
+            s = s.replaceAll("aya", " ");
+            s = s.replaceAll("ye", " ");
+            s = s.replaceAll("woo", " ");
+            s = s.replaceAll("ma", " ");
+
+            if (s.trim().length() == 0) answer++;
         }
-        
+
         return answer;
     }
 }
