60855daf52b7b769b634cf7c707be0076d420c26
hosu <76641246+junghojune@users.noreply.github.com>
2024-08-05 10:48:58 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        HashSet<Integer> reserveSet = new HashSet<>();
+        HashSet<Integer> lostSet = new HashSet<>();
+
+        for(int i : reserve){
+            reserveSet.add(i);
+        }
+        
+        for(int i : lost){
+            if(reserveSet.contains(i)){
+                reserveSet.remove(i);
+            } else {
+                lostSet.add(i);
+            }
+        }
+
+        
+        for(int i : reserveSet){
+            if(lostSet.contains(i - 1)){
+                lostSet.remove(i - 1);
+            } else if(lostSet.contains(i + 1)){
+                lostSet.remove(i + 1);
+            }
+        }
+        
+        int answer = n - lostSet.size();
+        
+        return answer;
+    }
+}
